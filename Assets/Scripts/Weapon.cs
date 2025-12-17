0bd997f3987db4f1325e21f58d01415229b7a30a
Rhiannon-code <rhipri@outlook.com.au>
2025-12-17 11:32:41 +1100

    Weapon Firing and Target hit added

---
+++ Assets/Scripts/Weapon.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+
+public GameObject bulletPrefab;
+public Transform bulletSpawn;
+public float bulletVelocity = 30;
+public float bulletPrefabLifeTime = 3f;
+
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            FireWeapon();
+        }
+    }
+
+    private void FireWeapon()
+    {
+        //Insantiate the Bullet
+        GameObject bullet = Instantiate(bulletPrefab, bulletSpawn.position, Quaternion.identity);
+        //Shoot Bullet
+        bullet.GetComponent<Rigidbody>().AddForce(bulletSpawn.forward.normalized * bulletVelocity, ForceMode.Impulse);
+        //Destroy Bullet After Some Time
+        StartCoroutine(DestroyBulletAfterTime(bullet, bulletPrefabLifeTime));
+
+
+    }
+
+    private IEnumerator DestroyBulletAfterTime(GameObject bullet, float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        Destroy(bullet);
+    }
+}
